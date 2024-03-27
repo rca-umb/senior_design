776f28a3b79a9b83744564f32dbbbdc886e43413
rca-umb <164802105+rca-umb@users.noreply.github.com>
2024-03-27 00:11:05 -0400

    Update rca_hub.py
    Accidently pasted twice on that last one.

--- rca_hub.py
+++ rca_hub.py
@@ -84,30 +84,3 @@ while True:
 	print("Drone 1: " + str(pred1*100) + "% Chance of Fire")
 	pred2 = make_prediction(drone_data[1])
 	print("Drone 2: " + str(pred2*100) + "% Chance of Fire")
-
-# Team 1 UMB Senior Design '23-'24
-# Updated code to take read data from drone XBees.
-
-from digi.xbee.devices import XBeeDevice, RemoteXBeeDevice, XBee64BitAddress
-import time
-
-# Instantiate a local XBee node (this is the XBee device connected to this Pi
-xbee = XBeeDevice("/dev/ttyUSB0", 115200)
-xbee.open()
-this_xbee = xbee.get_64bit_addr().address.hex() # 64bit address object -> bytearray -> string representation of hex value
-
-# Keep record of each XBee's address
-registry = {"0013a200420107ce": "Drone 1", "0013a200420107ef": "Drone 2", "0013a20042010691": "Hub"}
-
-print(registry[this_xbee] + ': Now Running')
-t = 1
-while True:
-	# Read incoming XBee data
-	try:
-		xbee_message = xbee.read_data(timeout=t)
-		that_xbee = xbee_message.remote_device.get_64bit_addr().address.hex()
-		print('From ' + registry[that_xbee] + ': ' + xbee_message.data)
-	except:
-		if (t < 120):
-			t = t + 1
-		print("No data received after " + str(t) + " seconds")
